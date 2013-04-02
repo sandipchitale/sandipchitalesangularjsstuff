aa2bcbcdb9e6d8b79ad88a9e4728b2f12ca265d1
Sandip Chitale <sandipchitale@gmail.com>
2013-04-01 21:21:36 -0700

    Correct the songs and band names.

--- ng-reorder/xng-reorderApp.js
+++ ng-reorder/xng-reorderApp.js
@@ -2,8 +2,8 @@ angular.module('xngreorderApp', ['xng']).controller('xngreorderController', func
     $scope.videos = [
         {style:{'background-image': "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1FWIoQPV69BkUhanIpMj8j-s-YkR97Wc8Gr9fPO0SNTk0WtTd_w')"}, name:'Time', band: "Pink Floyd"},
         {style:{'background-image': "url('https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRQ5vbbGOyGoGlZMal0ROKMufhE2M8oRisP9W1lvV0zue5V3d7Bog')"}, name:'Smoke on the water', band: "Deep Purple"},
-        {style:{'background-image': "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlbqGNwqveAiDGN_LJVKEk7f228r1HQlttu7_B3E3hosAQS_1Q'  )"}, name:'Nights on white satin', band: "Moody Blues"},
-        {style:{'background-image': "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAMbLIPLBo0EIaSvhSxQ63ZmNUxB7x0G1u0lhIrr4HuA8fv2yqTg')"}, name:'Nights on white satin', band: "Red Hot Chili Peppers"},
+        {style:{'background-image': "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlbqGNwqveAiDGN_LJVKEk7f228r1HQlttu7_B3E3hosAQS_1Q'  )"}, name:'Message in the bottle', band: "The Poilice"},
+        {style:{'background-image': "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAMbLIPLBo0EIaSvhSxQ63ZmNUxB7x0G1u0lhIrr4HuA8fv2yqTg')"}, name:'LA Woman', band: "Doors"},
         {style:{'background-image': "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbpd1_qOGK-Csg9p64LAQ86l3nzLOa8ghqeoS63X1auCRxcxW7'  )"}, name:'Yellow Submarine', band: "The Beatles"},
     ];
 });
